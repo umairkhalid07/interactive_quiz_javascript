dc0bf487956cb1880893d04f91ad9b6210aab417
umairkhalid07 <umairk911.uk@gmail.com>
2022-09-03 17:48:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const correctAnswers = ['A', 'B', 'A', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]){
+            score+=25;
+        }
+    });
+    // show result on page
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+});
+
+
